b2d6ab5b6a1ddcd6690bb943879f5e5881df2ef2
PoliTwit1984 <96096412+PoliTwit1984@users.noreply.github.com>
2022-07-10 18:55:06 -0500

    added delete image functionality

--- lpt_fetcher.py
+++ lpt_fetcher.py
@@ -3,7 +3,7 @@ import os
 import random
 import textwrap
 from string import ascii_letters
-import datetime 
+import datetime
 
 import praw
 import tweepy
@@ -64,23 +64,26 @@ class LPTFetcher:
         file_list = []
         for file in glob.glob("*.jpg"):
             file_list.append(file)
-
+        ''' Make sure there are files in the directory to tweet'''
         if len(file_list) > 0:
 
             random_image = random.choice(file_list)
             print(random_image)
+
             media = api.media_upload(random_image)
-            tweet_info = api.update_status(status=tweet_text, media_ids=[media.media_id])
+            tweet_info = api.update_status(
+                status=tweet_text, media_ids=[media.media_id])
             date_posted = datetime.datetime.now()
             if del_file:
                 os.remove(random_image)
-               
+            else:
+                lpt_tweet_file = open("lpt_tweet_file.txt", "a")
+                lpt_tweet_file.write(f"{date_posted}, {tweet_info.id}\n")
+
             tweeted = True
             print(tweet_info)
             print(tweet_info.id)
-            lpt_tweet_file = open("lpt_tweet_file.txt", "a")
-            lpt_tweet_file.write(f"{date_posted}, {tweet_info.id}\n")
-            
+
         else:
             print("No more images to tweet. Please add more images.")
             tweeted = False
@@ -90,9 +93,8 @@ class LPTFetcher:
 
 r = LPTFetcher(sub="lifeprotips", time_span="all", limit=365)
 
-# r.get_lpts()
-r.tweet_lpt_image(tweet_text = "Make your life easier by using #LifeProTips #LifeProTipofTheDay #lpt", del_file=False)
-
+r.tweet_lpt_image(
+    tweet_text="Make your life easier by using #LifeProTips #LifeProTipofTheDay #lpt", del_file=True)
 
 
 # 'all_awardings', 'allow_live_comments', 'approved_at_utc', 'approved_by', 'archived', 'author', 'author_flair_background_color', 'author_flair_css_class', 'author_flair_richtext', 'author_flair_template_id', 'author_flair_text', 'author_flair_text_color', 'author_flair_type', 'author_fullname', 'author_is_blocked', 'author_patreon_flair', 'author_premium', 'award', 'awarders', 'banned_at_utc', 'banned_by', 'can_gild', 'can_mod_post', 'category', 'clear_vote', 'clicked', 'comment_limit', 'comment_sort', 'comments', 'content_categories', 'contest_mode', 'created', 'created_utc', 'crosspost', 'delete', 'disable_inbox_replies', 'discussion_type', 'distinguished', 'domain', 'downs', 'downvote', 'duplicates', 'edit', 'edited', 'enable_inbox_replies', 'flair', 'fullname', 'gild', 'gilded', 'gildings', 'hidden', 'hide', 'hide_score', 'id', 'id_from_url', 'is_created_from_ads_ui', 'is_crosspostable', 'is_meta', 'is_original_content', 'is_reddit_media_domain', 'is_robot_indexable', 'is_self', 'is_video', 'likes', 'link_flair_background_color', 'link_flair_css_class', 'link_flair_richtext', 'link_flair_template_id', 'link_flair_text', 'link_flair_text_color', 'link_flair_type', 'locked', 'mark_visited', 'media', 'media_embed', 'media_only', 'mod', 'mod_note', 'mod_reason_by', 'mod_reason_title', 'mod_reports', 'name', 'no_follow', 'num_comments', 'num_crossposts', 'num_reports', 'over_18', 'parent_whitelist_status', 'parse', 'permalink', 'pinned', 'pwls', 'quarantine', 'removal_reason', 'removed_by', 'removed_by_category', 'reply', 'report', 'report_reasons', 'save', 'saved', 'score', 'secure_media', 'secure_media_embed', 'selftext', 'selftext_html', 'send_replies', 'shortlink', 'spoiler', 'stickied', 'subreddit', 'subreddit_id', 'subreddit_name_prefixed', 'subreddit_subscribers', 'subreddit_type', 'suggested_sort', 'thumbnail', 'thumbnail_height', 'thumbnail_width', 'title', 'top_awarded_type', 'total_awards_received', 'treatment_tags', 'unhide', 'unsave', 'ups', 'upvote', 'upvote_ratio', 'url', 'user_reports', 'view_count', 'visited', 'whitelist_status', 'wls']
